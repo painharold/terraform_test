f161685f4a542aede18e4d25b103f4ae99ebd7df
Dmitry Maslov <1637942@gmail.com>
2023-05-30 17:01:19 +0200

    db added

--- instances.tf
+++ instances.tf
@@ -59,6 +59,31 @@ resource "aws_security_group" "sg-wp" {
   }
 }
 
+# Create a security group for db
+resource "aws_security_group" "sg-db" {
+  name   = "DB Security Group"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.sg-wp.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Security Group DB"
+    Project = "Terraform Test Project"
+  }
+}
+
 #-------------Instances----------------------------------------
 
 # Create webservers
@@ -110,4 +135,52 @@ resource "aws_elb_attachment" "elb" {
   count = length(aws_instance.webserver.*.id)
   elb      = aws_elb.elb.id
   instance = element(aws_instance.webserver.*.id, count.index)
+}
+
+#-------------Data Base----------------------------------------
+
+resource "random_string" "rds_password" {
+  length = 12
+  special = true
+  override_special = "!#$&"
+}
+
+resource "aws_ssm_parameter" "rds_password" {
+  name  = "/test_project/mysql"
+  type  = "SecureString"
+  value = random_string.rds_password.result
+  description = "Master Password for RDS MySQL"
+}
+
+data "aws_ssm_parameter" "my_rds_password" {
+  name = "/test_project/mysql"
+  depends_on = [aws_ssm_parameter.rds_password]
+}
+
+resource "aws_db_subnet_group" "db_subnets" {
+  name       = "main"
+  subnet_ids = aws_subnet.private.*.id
+
+  tags = {
+    name = "DB Subnet Group"
+    Project = "Terraform Test Project"
+  }
+}
+
+resource "aws_db_instance" "db-mysql" {
+  identifier = "test-project-rds"
+  allocated_storage = 20
+  storage_type = "gp2"
+  engine = "mysql"
+  engine_version = "5.7"
+  instance_class = "db.t2.micro"
+  db_name = "wp_db"
+  db_subnet_group_name = aws_db_subnet_group.db_subnets.id
+  vpc_security_group_ids = [aws_security_group.sg-db.id]
+  port = "3306"
+  username = "administrator"
+  password = data.aws_ssm_parameter.my_rds_password.value
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot = true
+  apply_immediately = true
 }
