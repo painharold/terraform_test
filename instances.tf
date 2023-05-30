1821b5cffd30d43524c7e6de00916fff1899f5fe
Dmitry Maslov <1637942@gmail.com>
2023-05-30 17:02:11 +0200

    db added

--- instances.tf
+++ instances.tf
@@ -183,4 +183,9 @@ resource "aws_db_instance" "db-mysql" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot = true
   apply_immediately = true
+
+  tags = {
+    name = "DB Subnet Group"
+    Project = "Terraform Test Project"
+  }
 }
