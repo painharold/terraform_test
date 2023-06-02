2ed409d9c936264924e6016d7c35e67c5cdf5258
Dmitry Maslov <1637942@gmail.com>
2023-06-02 13:45:56 +0200

    fix instance

--- terraform/test_project/db.tf
+++ terraform/test_project/db.tf
@@ -43,7 +43,7 @@ resource "aws_db_instance" "db-mysql" {
   storage_type           = "gp2"
   engine                 = "mysql"
   engine_version         = "5.7"
-  instance_class         = "db.t2.micro"
+  instance_class         = "db.t3.micro"
   db_name                = "wp_db"
   db_subnet_group_name   = aws_db_subnet_group.db_subnets.id
   vpc_security_group_ids = [aws_security_group.sg-db.id]
